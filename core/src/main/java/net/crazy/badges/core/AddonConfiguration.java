fee70da540adab56b3b2671c3883ff2a3a5a0971
CrazySchnetzler1 <jan.web@outlook.de>
2022-08-23 12:55:43 +0200

    Added settings for the size of the Badges and if the player should see their own Badges

--- core/src/main/java/net/crazy/badges/core/AddonConfiguration.java
+++ core/src/main/java/net/crazy/badges/core/AddonConfiguration.java
@@ -1,19 +1,34 @@
-package org.example.core;
+package net.crazy.badges.core;
 
 import net.labymod.api.addon.AddonConfig;
+import net.labymod.api.client.gui.screen.widget.widgets.input.SliderWidget.SliderSetting;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
 import net.labymod.api.configuration.loader.annotation.ConfigName;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
 
 @SuppressWarnings("FieldMayBeFinal")
 @ConfigName("settings")
-public class ExampleConfiguration extends AddonConfig {
+public class AddonConfiguration extends AddonConfig {
 
   @SwitchSetting
   private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
 
+  @SliderSetting(min = 5, max = 20)
+  private final ConfigProperty<Integer> size = new ConfigProperty<>(10);
+
+  @SwitchSetting
+  private final ConfigProperty<Boolean> showOwn = new ConfigProperty<>(true);
+
   @Override
   public ConfigProperty<Boolean> enabled() {
     return this.enabled;
+  }
+
+  public int size() {
+    return this.size.get();
+  }
+
+  public boolean showOwn() {
+    return this.showOwn.get();
   }
 }
