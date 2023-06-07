18e3530a3e383f89760a844e15ea1c976c59c269
Crazy <contact@csjako.de>
2023-06-07 10:13:02 +0200

    Removed deprecated method

--- core/src/main/java/net/crazy/badges/core/activities/BadgeActivity.java
+++ core/src/main/java/net/crazy/badges/core/activities/BadgeActivity.java
@@ -10,20 +10,18 @@ import net.labymod.api.client.gui.screen.activity.Link;
 import net.labymod.api.client.gui.screen.widget.action.ListSession;
 import net.labymod.api.client.gui.screen.widget.widgets.layout.ScrollWidget;
 import net.labymod.api.client.gui.screen.widget.widgets.layout.TilesGridWidget;
-import org.jetbrains.annotations.Nullable;
 import java.util.LinkedList;
 
 @AutoActivity
 @Link("overview.lss")
 public class BadgeActivity extends Activity {
-  private final Badges addon;
 
-  private LinkedList<BadgeWidget> badgeWidgets = new LinkedList<>();
-  private TilesGridWidget<BadgeWidget> gridWidget;
-  private ListSession<?> listSession = new ListSession<>();
+  private final LinkedList<BadgeWidget> badgeWidgets = new LinkedList<>();
+  private final TilesGridWidget<BadgeWidget> gridWidget;
+  private final ListSession<?> listSession = new ListSession<>();
 
   public BadgeActivity() {
-    this.addon = Badges.addon;
+    Badges addon = Badges.addon;
 
     for (Badge badge : addon.badges.values())
       badgeWidgets.add(new BadgeWidget(badge.icon(), badge.getName(), badge.getDescription()));
@@ -42,10 +40,5 @@ public class BadgeActivity extends Activity {
 
     ScrollWidget scrollWidget = new ScrollWidget(gridWidget, listSession);
     document().addChild(scrollWidget);
-  }
-
-  @Override
-  public <T extends LabyScreen> @Nullable T renew() {
-    return new BadgeActivity().generic();
   }
 }
